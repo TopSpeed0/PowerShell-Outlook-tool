cdee054256b6898ae1419032ffc18f16aaa686d2
Bohadana, Yitzhak <Yitzhak.Bohadana@cognyte.com>
2026-05-17 17:07:22 +0300

    Remove obsolete scripts and binaries from the repository

--- Update Build.ps1
+++
@@ -1,7 +0,0 @@
-# Ensure this script runs in PowerShell Core
-if ($PSVersionTable.PSVersion.Major -lt 7) {
-    Write-Host "This script requires PowerShell Core (version 7 or higher). Exiting."
-    exit 1
-}
-cd "C:\Users\ybohadana\OneDrive - COGNYTE\Documents\code\Cognyte\Microsoft\Outlook\Restore-MSG"
-Invoke-ps2exe -inputFile .\Restore-MSG.ps1 -outputFile .\Restore-MSG.exe -title "Restore MSG" -iconFile .\Designer.ico -company Cognyte -description "Restore MSG base file to outlook" -version 1.1.1 -copyright Cognyte
